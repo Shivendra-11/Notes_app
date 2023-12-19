8e722e2d2676c2e898ed91eee1a44e5cd30ea209
shivendra <shivendrakeshari11@gmail.com>
2023-12-20 01:36:45 +0530

    completed

---
+++ script.js
@@ -0,0 +1,60 @@
+const notesContainer=document.querySelector(".notes-conatiner");
+const createBtn=document.querySelector(".btn");
+let notes=document.querySelectorAll(".input-box");
+
+function showNotes(){
+    notesContainer.innerHTML=localStorage.getItem("notes")
+}
+
+function update(){
+    localStorage.setItem("notes"  ,notesContainer.innerHTML)
+}
+
+
+createBtn.addEventListener("click",()=>{
+    let inputBox=document.createElement("p");
+    let img=document.createElement("img");
+    inputBox.className="input-box";
+    inputBox.setAttribute("contenteditable"  , "true" );
+    img.src="images/delete.png";
+    notesContainer.appendChild(inputBox).appendChild(img);
+})
+
+notesContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "IMG") {
+        e.target.parentElement.remove();
+        update();
+    }
+
+    else if(e.target.tagName==="P"){
+        notes.document.querySelectorAll(".input-box");
+
+        notes.forEach(nt=>{
+            nt.onKeyup=function(){
+               update(); 
+            }
+        })
+
+    }
+});
+
+document.addEventListener("keydown", e => {
+    if (e.key === "Enter") {
+        document.execCommand("insertLinebreak");
+        e.preventDefault();
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
